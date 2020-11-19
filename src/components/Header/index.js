d746f250cbe88424e0a6749a617d51265a59d4c0
dayana-sog <dayanasog23@gmail.com>
2020-11-19 12:56:43 +0000

    Commit inicial

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Header() {
+  return (
+    <Container>
+      <h1>Resiliência é importantet para os Devs?</h1>
+
+      <ul>
+        <li>
+          <img src="" alt="1photo" />
+          <p>O que é resiliência?</p>
+        </li>
+
+        <li>
+          <img src="" alt="1photo" />
+          <p>Como melhorar a resiliência?</p>
+        </li>
+
+        <li>
+          <img src="" alt="1photo" />
+          <p>Meu depoimento:</p>
+        </li>
+      </ul>
+    </Container>
+  );
+}
+
+export default Header;
